d39f527ee7cd2653c2527152d13ca7fbd4078ff4
Dung2781993@gmail.com <Dung2781993@gmail.com>
2022-03-15 01:31:06 +1100

    Database first

---
+++ DatabaseFirst/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new BikeStoresEntities();
+            var products = from p in db.products select p; 
+            foreach(var item in products)
+            {
+                Console.WriteLine("ID {0}", item.product_id);
+                Console.WriteLine("Product Name {0}", item.product_name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
