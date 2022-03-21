7fc528eb5c6b8bb50d9ea6942e4b40fbba667949
Dung2781993@gmail.com <Dung2781993@gmail.com>
2022-03-22 00:53:57 +1100

    Update Patient Generator

---
+++ Hospital/Hospital/Seeding/AddressGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hospital.Seeding
+{
+    public class AddressGenerator
+    {
+        private static Random rnd = new Random();
+
+        private static string[] townNames =
+        {
+            "Test1",
+            "Test2 ",
+            "Test3"
+        };
+        //private static string[] townNames = File.ReadAllLines("<INSERT DIR HERE>");
+        private static string[] streetNames =
+        {
+            "Stree1",
+            "Stree2",
+            "Stree3"
+        };
+        //private static string[] streetNames = File.ReadAllLines("<INSERT DIR HERE>");
+
+        internal static string NewAddress()
+        {
+            string townName = townNames[rnd.Next(townNames.Length)];
+            string streetName = streetNames[rnd.Next(streetNames.Length)];
+            int number = rnd.Next(1, 100);
+
+            return $"{townName}, {streetName} {number}";
+        }
+    }
+}
--- Hospital/Hospital/Seeding/PatientGenerator.cs
+++ Hospital/Hospital/Seeding/PatientGenerator.cs
@@ -16,15 +16,77 @@ namespace Hospital.Seeding
             string firstName = NameGenerator.FirstName();
             string lastName = NameGenerator.LastName();
             string email = EmailGenerator.GenerateEmail(firstName + lastName);
+            string address = AddressGenerator.NewAddress();
 
             var patient = new Patient()
             {
                 FirstName = firstName,
                 LastName = lastName,
-                Email = email
-            }
+                Email = email,
+                Address = address
+            };
+            patient.Visitations = GenerateVisitations(patient);
+            patient.Diagnoses = GenerateDiagnoses(patient);
+
+            return patient;
         }
 
+        private static Diagnose[] GenerateDiagnoses(Patient patient)
+        {
+            var diagnoseNames = new string[]
+            {
+                "Lung Cancer",
+                "Heart Attack",
+                "Strock",
+                "Covid",
+                "HIV"
+            };
 
+            int diagnoseCount = random.Next(1, 4);
+            var diagnoses = new Diagnose[diagnoseCount];
+            for (int i = 0; i < diagnoseCount; i++)
+            {
+                string diagnoseName = diagnoseNames[random.Next(diagnoseNames.Length)];
+
+                var diagnose = new Diagnose()
+                {
+                    Name = diagnoseName,
+                    Patient = patient
+                };
+
+                diagnoses[i] = diagnose;
+            }
+
+            return diagnoses;
+        }
+
+        private static Visitation[] GenerateVisitations(Patient patient)
+        {
+            int visitationCount = random.Next(1, 5);
+
+            var visitations = new Visitation[visitationCount];
+
+            for (int i = 0; i < visitationCount; i++)
+            {
+                var visitationDate = RandomDay(2005);
+
+                var visitation = new Visitation()
+                {
+                    Date = visitationDate,
+                    Patient = patient
+                };
+
+                visitations[i] = visitation;
+            }
+
+            return visitations;
+        }
+
+        private static DateTime RandomDay(int startYear)
+        {
+            DateTime start = new DateTime(startYear, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(random.Next(range));
+        }
     }
 }
